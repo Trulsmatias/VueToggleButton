48b722467b486bc7a9a771bf8ca88c3ab3649e8c
Truls Matias Gederø Torgersen <trulsmatias@gmail.com>
2023-03-25 15:49:26 +0100

    removed starting code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-import type ChatToggleVue from './components/ChatToggle.vue';
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import ToggleItem from './components/ToggleItem.vue'
-import ChatToggle from './components/ChatToggle.vue';
-import TuturialToggle from './TuturialToggle.vue';
+import Toggle from './components/Toggle.vue';
 import { ref } from 'vue'
 const leftLabelValue = "leftString"
 const rightLabelValue = "rightString"
@@ -13,48 +8,9 @@ const isToggleEnabled = true;
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it too!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-    <ToggleItem :leftLabel="leftLabelValue" v-bind:right-label="rightLabelValue" />
-    <TuturialToggle :value="toggleValue" @emittedValued="(val) => toggleValue = val" :left-label="leftLabelValue"
-      :right-label="rightLabelValue" :isEndabled="isToggleEnabled" />
-    <p>{{ toggleValue }}</p>
-  </main>
+  <Toggle :value="toggleValue" @emittedValued="(val) => toggleValue = val" :left-label="leftLabelValue"
+    :right-label="rightLabelValue" :isEndabled="isToggleEnabled" />
+  <p>{{ toggleValue }}</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    /*flex-wrap: wrap;*/
-  }
-}
-</style>
+<style lang="scss"></style>
